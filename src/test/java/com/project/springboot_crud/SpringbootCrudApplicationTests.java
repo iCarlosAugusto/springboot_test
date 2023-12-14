a662abfa69508cfab54f3b55099f35422f5ee3b5
augusto.dev@proton.me <augusto.dev@proton.me>
2023-12-14 14:43:45 -0300

    feat: init

---
+++ src/test/java/com/project/springboot_crud/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.springboot_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
