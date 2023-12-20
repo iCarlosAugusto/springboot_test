29ab58f3cf3822756658e06b56e3bc378bb9fcb2
Carlos Augusto de Lima Vieira <carlos.vieira@opah.com.br>
2023-12-20 18:44:15 -0300

    organization

--- src/main/java/com/project/springboot_crud/configs/WebConfiguration.java
+++ src/main/java/com/project/springboot_crud/configs/WebConfiguration.java
@@ -1,4 +1,4 @@
-package com.project.springboot_crud;
+package com.project.springboot_crud.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
